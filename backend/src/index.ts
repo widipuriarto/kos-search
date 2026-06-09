e2035332c1839974013a10943c458410e1b5108f
Widi Puriarto <widipuriarto@gmail.com>
2026-06-10 05:27:49 +0700

    update configuration for deploy vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,6 +35,10 @@ app.use('/api/chat', chatRoutes);
 // Middleware Error Handler harus di paling bawah
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
